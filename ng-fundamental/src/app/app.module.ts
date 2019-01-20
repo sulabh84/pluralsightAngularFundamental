b491e693a780f9a013d81071b4c22f406396608e
sulabh <sulabh.a2@gmail.com>
2019-01-21 01:17:51 +0530

    routes added

--- ng-fundamental/src/app/app.module.ts
+++ ng-fundamental/src/app/app.module.ts
@@ -7,16 +7,21 @@ import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { NavBarComponent } from './nav/navbar.component'
 import { EventService } from './events/shared/event.service'
 import { ToastrService } from './common/toastr.service'
+import { EventDetailsComponent } from './events/event-details/event-details.component'
+import { RouterModule } from '@angular/router'
+import { appRoutes } from '../routes'
 
 @NgModule({
   declarations: [
     EventsAppComponent,
     EventsListComponent,
     EventThumbnailComponent,
-    NavBarComponent
+    NavBarComponent,
+    EventDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     EventService,
---
+++ ng-fundamental/src/app/events/event-details/event-details.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core'
+import { EventService } from '../shared/event.service'
+import { ActivatedRoute } from '@angular/router'
+
+@Component({
+  selector: 'event-details',
+  templateUrl: './event-details.component.html'
+})
+
+export class EventDetailsComponent{
+
+  event:any
+
+  constructor(private eventService:EventService, private route:ActivatedRoute){}
+
+  ngOnInit(){
+    this.event = this.eventService.getEvent(+this.route.snapshot.params['id'])
+  }
+
+}
