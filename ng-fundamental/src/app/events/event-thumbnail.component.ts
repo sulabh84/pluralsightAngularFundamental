455537e567f22b3ddfae6fcf630845289886887a
sulabh <sulabh.a2@gmail.com>
2019-01-25 01:25:44 +0530

    type safe interfaces

--- ng-fundamental/src/app/events/event-thumbnail.component.ts
+++ ng-fundamental/src/app/events/event-thumbnail.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core'
+import { IEvent } from './shared/event.model'
 
 @Component({
   selector: 'event-thumbnail',
@@ -19,7 +20,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
 })
 
 export class EventThumbnailComponent {
-  @Input() event:any
+  @Input() event:IEvent
   @Output() eventClick = new EventEmitter()
   someproperty:any = "some value"
 
--- ng-fundamental/src/app/events/events-list.component.ts
+++ ng-fundamental/src/app/events/events-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core'
 import { EventService } from './shared/event.service'
 import { ToastrService } from '../common/toastr.service'
 import { ActivatedRoute } from '@angular/router'
+import { IEvent } from './shared/event.model'
 
 declare let toastr
 
@@ -25,7 +26,7 @@ declare let toastr
   `
 })
 export class EventsListComponent implements OnInit {
-  events:any
+  events:IEvent[]
 
   constructor(private eventService:EventService, private toastrService:ToastrService,
                   private activatedRoute:ActivatedRoute){
