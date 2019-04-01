b558d5222a2c4eeec5d85714be6f85bc5b4a3a30
sulabh <sulabh.a2@gmail.com>
2019-04-01 23:56:59 +0530

    child to parent data/event passing

--- ng-fundamental/src/app/events/event-details/create-session.component.ts
+++ ng-fundamental/src/app/events/event-details/create-session.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, Output, EventEmitter } from '@angular/core'
 import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { ISession } from '../shared/event.model'
 import { restrictedWords } from '../shared/restricted.words.validator'
 
 @Component({
+  selector: 'create-session',
   templateUrl: './create-session.component.html'
 })
 
 export class CreateSessionComponent implements OnInit {
+  @Output() saveNewSession = new EventEmitter()
+  @Output() cancelEvent = new EventEmitter()
   newSessionForm: FormGroup
   name : FormControl
   presenter: FormControl
@@ -43,5 +46,10 @@ export class CreateSessionComponent implements OnInit {
       voters: []
     }
     console.log(session)
+    this.saveNewSession.emit(session)
+  }
+
+  cancel(){
+    this.cancelEvent.emit()
   }
 }
--- ng-fundamental/src/app/events/event-details/event-details.component.ts
+++ ng-fundamental/src/app/events/event-details/event-details.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core'
 import { EventService } from '../shared/event.service'
 import { ActivatedRoute } from '@angular/router'
 import { IEvent } from '../shared/event.model'
+import { ISession } from '../shared/event.model'
 
 @Component({
   selector: 'event-details',
@@ -11,6 +12,7 @@ import { IEvent } from '../shared/event.model'
 export class EventDetailsComponent{
 
   event:IEvent
+  addMode: boolean
 
   constructor(private eventService:EventService, private route:ActivatedRoute){}
 
@@ -18,4 +20,19 @@ export class EventDetailsComponent{
     this.event = this.eventService.getEvent(+this.route.snapshot.params['id'])
   }
 
+  addSession(){
+    this.addMode = true
+  }
+
+  saveNewSession(session:ISession){
+    const nextId = Math.max.apply(null, this.event.sessions.map(s => s.id))
+    session.id = nextId + 1
+    this.event.sessions.push(session)
+    this.eventService.updateEvent(this.event)
+    this.addMode = false
+  }
+
+  cancel(){
+    this.addMode = false
+  }
 }
